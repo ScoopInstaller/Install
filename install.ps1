2e5efa43847d111cb91ad0589fa1768bfc1785b1
Chawye Hsu <su+git@chawyehsu.com>
2026-08-26 18:31:41 +0800

    fix: validate directories before install
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- install.ps1
+++ install.ps1
@@ -131,6 +131,31 @@ function Test-ValidateParameter {
     if ($ProxyUseDefaultCredentials -and $null -ne $ProxyCredential) {
         Deny-Install "ProxyUseDefaultCredentials is conflict with ProxyCredential. Don't use the -ProxyCredential and -ProxyUseDefaultCredentials together."
     }
+
+    # Avoid installing Scoop to a path containing spaces
+    if ($SCOOP_DIR.Contains(' ')) {
+        Deny-Install "Installing Scoop to path '$SCOOP_DIR' containing spaces may cause unexpected behaviors, please specify another path."
+    }
+
+    if (Test-Path $SCOOP_DIR -PathType Leaf) {
+        Deny-Install "'$SCOOP_DIR' is a file, please remove it or specify another path."
+    }
+
+    if ((Test-Path $SCOOP_DIR -PathType Container) -and [bool](Get-ChildItem $SCOOP_DIR -ErrorAction SilentlyContinue)) {
+        Deny-Install "'$SCOOP_DIR' exists and is not empty, please specify another path."
+    }
+
+    if (Test-Path $SCOOP_GLOBAL_DIR -PathType Leaf) {
+        Deny-Install "'$SCOOP_GLOBAL_DIR' is a file, please remove it or specify another path."
+    }
+
+    if ((Test-Path $SCOOP_GLOBAL_DIR -PathType Container) -and [bool](Get-ChildItem $SCOOP_GLOBAL_DIR -ErrorAction SilentlyContinue)) {
+        Deny-Install "'$SCOOP_GLOBAL_DIR' exists and is not empty, please specify another path."
+    }
+
+    if (Test-Path $SCOOP_CACHE_DIR -PathType Leaf) {
+        Deny-Install "'$SCOOP_CACHE_DIR' is a file, please remove it or specify another path."
+    }
 }
 
 function Test-IsAdministrator {
